12c317c94a55ecf49e997d5ad9ab047c6fa6e934
Benjamin Desef <projekter-git@yahoo.de>
2025-05-03 11:38:05 +0200

    Fix KillConjugates

--- src/newton/helpers/KillConjugates.jl
+++ src/newton/helpers/KillConjugates.jl
@@ -2,9 +2,12 @@ struct KillConjugates{Nr,P}
     parent::P
 end
 
+KillConjugates{Nr}(parent::P) where {Nr,P} = KillConjugates{Nr,P}(parent)
+
 Base.IteratorSize(::Type{<:KillConjugates{<:Any,P}}) where {P} = Base.IteratorSize(P)
 Base.IteratorEltype(::Type{<:KillConjugates{<:Any,P}}) where {P} = Base.HasEltype()
 Base.length(k::KillConjugates) = length(k.parent)
+Base.size(k::KillConjugates) = size(k.parent)
 Base.eltype(::Type{<:KillConjugates{<:Any,P}}) where {P} = eltype(P)
 function Base.iterate(k::KillConjugates{Nr}) where {Nr}
     result = iterate(k.parent)
@@ -15,8 +18,8 @@ function Base.iterate(k::KillConjugates, (take, state))
     result = iterate(k.parent, state)
     isnothing(result) && return nothing
     if iszero(take)
-        return zero(result[1]), (one(take), state)
+        return zero(result[1]), (one(take), result[2])
     else
-        return result[1], (take - one(take), state)
+        return result[1], (take - one(take), result[2])
     end
 end
