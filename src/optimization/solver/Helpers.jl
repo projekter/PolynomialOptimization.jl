d44b36f514394e690eb715216b003906c58e04ea
Benjamin Desef <projekter-git@yahoo.de>
2025-06-07 16:23:04 +0200

    Generalize ScalarMatrix to ScalarArray

--- src/optimization/solver/Helpers.jl
+++ src/optimization/solver/Helpers.jl
@@ -85,19 +85,24 @@ end
     end
 end
 
-struct ScalarMatrix{X} <: AbstractMatrix{X} # we could make it mutable and fully implement the AbstractArray interface, but we
-                                            # don't need it
+struct ScalarArray{X,N} <: AbstractArray{X,N} # we could make it mutable and fully implement the AbstractArray interface, but
+                                              # we don't need it
     x::X
 end
 
-Base.size(::ScalarMatrix) = (1, 1)
-Base.length(::ScalarMatrix) = 1
-@inline function Base.getindex(m::ScalarMatrix, args...)
+ScalarArray{<:Any,N}(x::X) where {X,N} = ScalarArray{X,N}(x)
+
+Base.size(::ScalarArray{<:Any,N}) where {N} = ntuple(_ -> 1, Val(N))
+Base.length(::ScalarArray) = 1
+@inline function Base.getindex(m::ScalarArray, args...)
     @boundscheck checkbounds(m, args...)
     return m.x
 end
-Base.iterate(m::ScalarMatrix) = m.x, nothing
-Base.iterate(::ScalarMatrix, ::Nothing) = nothing
+Base.iterate(m::ScalarArray) = m.x, nothing
+Base.iterate(::ScalarArray, ::Nothing) = nothing
+
+const ScalarVector{X} = ScalarArray{X,1}
+const ScalarMatrix{X} = ScalarArray{X,2}
 
 """
     count_uniques(vec::AbstractVector[, callback])
